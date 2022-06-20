ff10d78e9ff62a344b249a2d2b0e2aeb145df5df
Royalboe <auomidejohnson@gmail.com>
2022-06-20 13:31:57 +0100

    Added UI and styles

--- script.js
+++ script.js
@@ -17,38 +17,34 @@ let computerSelection = computerChoice(computerPlay)
 let computerScore = 0;
 let playerScore = 0;
 
-function gameOn() {
-    let getWinner = (computerSelection, playerSelection) => {
-        if (computerSelection === 'Rock' && playerSelection.toUpperCase() === "ROCK" 
-        || computerSelection === 'Paper' && playerSelection.toUpperCase() === "PAPER"
-        || computerSelection === 'Scissors' && playerSelection.toUpperCase() === "SCISSORS") {
-            return "That is a tie!"
-        } else if (computerSelection === 'Rock' && playerSelection.toUpperCase() === "SCISSORS") {
-            computerScore++
-            return "Computer wins, you chose Scissors and computer chose Rock"
-        } else if (computerSelection === 'Scissors' && playerSelection.toUpperCase() === "PAPER") {
-            computerScore++
-            return "Computer wins, you chose Paper and computer chose Scissors"
-        } else if (computerSelection === 'Paper' && playerSelection.toUpperCase() === "ROCK") {
-            computerScore++
-            return "Opps. you lost. Computer chose Paper and you chose Rock"
-        }  else if (computerSelection === 'Scissors' && playerSelection.toUpperCase() === "ROCK") {
-            playerScore++
-            return "Computer wins, you chose Scissors and computer chose Rock"
-        } else if (computerSelection === 'Paper' && playerSelection.toUpperCase() === "SCISSORS") {
-            playerScore++
-            return "Computer wins, you chose Paper and computer chose Scissors"
-        } else if (computerSelection === 'Rock' && playerSelection.toUpperCase() === "PAPER") {
-            playerScore++
-            return "Opps. you lost. Computer chose Paper and you chose Rock"
-        }
+let getWinner = (computerSelection, playerSelection) => {
+    if (computerSelection === 'Rock' && playerSelection.toUpperCase() === "ROCK" 
+    || computerSelection === 'Paper' && playerSelection.toUpperCase() === "PAPER"
+    || computerSelection === 'Scissors' && playerSelection.toUpperCase() === "SCISSORS") {
+        return "That is a tie!"
+    } else if (computerSelection === 'Rock' && playerSelection.toUpperCase() === "SCISSORS") {
+        computerScore++
+        return "Computer wins, you chose Scissors and computer chose Rock"
+    } else if (computerSelection === 'Scissors' && playerSelection.toUpperCase() === "PAPER") {
+        computerScore++
+        return "Computer wins, you chose Paper and computer chose Scissors"
+    } else if (computerSelection === 'Paper' && playerSelection.toUpperCase() === "ROCK") {
+        computerScore++
+        return "Opps. you lost. Computer chose Paper and you chose Rock"
+    }  else if (computerSelection === 'Scissors' && playerSelection.toUpperCase() === "ROCK") {
+        playerScore++
+        return "Computer wins, you chose Scissors and computer chose Rock"
+    } else if (computerSelection === 'Paper' && playerSelection.toUpperCase() === "SCISSORS") {
+        playerScore++
+        return "Computer wins, you chose Paper and computer chose Scissors"
+    } else if (computerSelection === 'Rock' && playerSelection.toUpperCase() === "PAPER") {
+        playerScore++
+        return "Opps. you lost. Computer chose Paper and you chose Rock"
     }
-    console.log(`${getWinner(computerSelection, playerSelection)}`)
 }
 
-let i;
-for (i = 0; i < 5; i++){
-    gameOn()
+function gameOn() {
+    console.log(`${getWinner(computerSelection, playerSelection)}`)
 }
 
 let winner = playerScore > computerScore ? "You win" : playerScore === computerScore ? "It is a tie" : "Computer wins"
